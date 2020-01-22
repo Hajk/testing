0d9017b04d8e26cbaf7009ece1040754e81e270c
Hajk <h.hajk@hotmail.com>
2020-01-22 09:51:07 +0100

    bro added

--- testing/lambda.js
+++ testing/lambda.js
@@ -24,5 +24,5 @@ exports.handler = function (event, context, callback) {
         });
 
 
-    callback(null, { "message": "Successfully executed" });
+    callback(null, { "message": "Successfully executed bro" });
 }
